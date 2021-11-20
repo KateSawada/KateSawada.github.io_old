f1782501b2b4f95404b6a9e80f00a40c17e5b3b2
KateSawada <nuksawada@gmail.com>
2021-11-20 16:39:49 +0900

    update award histry

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -4,6 +4,11 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 
+/**
+ * デプロイ: yarn run deploy
+ * ローカルサーバー gatsby develop
+ */
+
 const IndexPage = () => (
   <Layout>
     <div class="h-screen w-screen bg-gray-400 flex items-center justify-center ">
@@ -22,6 +27,7 @@ const IndexPage = () => (
               <tr><td>2019.4</td><td>名古屋大学 情報学部 コンピュータ科学科 入学</td></tr>
               <tr><td>2020.1 - 2021.3</td><td>株式会社キャリアビジョン 社内向けツール作成アルバイト</td></tr>
               <tr><td>2021.4 - </td><td>名古屋大学 空手同好会 主将</td></tr>
+              <tr><td>2021.4 - </td><td>名古屋大学卓越大学院 未来エレクトロニクス創成加速DII協働大学院プログラム チームopensesame</td></tr>
               <tr><td>2021.5 - </td><td>東海国立大学機構 情報推進部 情報基盤課 情報システムグループ 技術補佐員</td></tr>
             </table>
           </div>
@@ -48,6 +54,7 @@ const IndexPage = () => (
             <table class="xl:mr-56 xl:ml-56 lg:mr-40 lg:ml-40 mr-10 ml-10">
               <tr><td>2021.6</td><td>Tongaliビジネスプランコンテスト2021 大和証券賞</td></tr>
               <tr><td>2021.8</td><td>お盆deハッカソン 協賛企業賞</td></tr>
+              <tr><td>2021.11</td><td>JPHACKS2021 プラチナスポンサー賞 バンダイナムコ研究所賞</td></tr>
             </table>
           </div>
         </div>
