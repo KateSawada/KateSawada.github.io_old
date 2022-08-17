bdf411dc23cbb707fae1f749aa42ad6155fdc907
KateSawada <nuksawada@gmail.com>
2022-08-18 00:20:19 +0900

    Deploy to production

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,11 +4,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-404-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/page-2.js")),
-  "component---src-pages-skills-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/skills.js")),
-  "component---src-pages-test-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/test.js")),
-  "component---src-pages-using-typescript-tsx": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio/src/pages/using-typescript.tsx"))
+  "component---src-pages-404-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/page-2.js")),
+  "component---src-pages-skills-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/skills.js")),
+  "component---src-pages-test-js": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/test.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/mnt/hdd/keito/documents/project/portfolio_kai/src/pages/using-typescript.tsx"))
 }
 
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,14 +1,14 @@
 var plugins = [{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/mnt/hdd/keito/documents/project/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/mnt/hdd/keito/documents/project/portfolio_kai/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-image',
-      plugin: require('/mnt/hdd/keito/documents/project/portfolio/node_modules/gatsby-plugin-image/gatsby-ssr'),
+      plugin: require('/mnt/hdd/keito/documents/project/portfolio_kai/node_modules/gatsby-plugin-image/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-manifest',
-      plugin: require('/mnt/hdd/keito/documents/project/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/mnt/hdd/keito/documents/project/portfolio_kai/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
     }]
 /* global plugins */
